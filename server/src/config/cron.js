6f4e98d3a2993820c032868b569b24855fce8af7
Bruno Hirae <brunodeluca117@outlook.com>
2025-10-13 16:11:18 -0300

    feat: add cron job for periodic GET requests to external API

---
+++ server/src/config/cron.js
@@ -0,0 +1,13 @@
+import cron from 'cron';
+import https from 'https';
+
+const job = new cron.CronJob('*/14 * * * *', function() {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log('GET request sent successfully');
+      else console.error(`GET request failed with status code: ${res.statusCode}`);
+    })
+    .on('error', (e) => console.error(`Error making GET request: ${e.message}`));
+});
+
+export default job;
--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,12 @@ import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const app = express();
 app.use(express.json());
+
+if (ENV.NODE_ENV === 'production') job.start();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
